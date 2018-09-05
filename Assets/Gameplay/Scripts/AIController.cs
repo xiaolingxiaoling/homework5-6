518461e96e554b4b78b4cbbea7289ee9693259d4
Your Name <2453159355@qq.com>
2018-09-05 12:52:36 +0800

    first commit

---
+++ Assets/Gameplay/Scripts/AIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+public class AIController : MonoBehaviour {
+
+	NavMeshAgent agent;
+	PlayerCharacter character;
+	Transform player;
+
+
+	void Start () 
+	{
+		character = GetComponent<PlayerCharacter>();
+		agent = GetComponent<NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag ("Player"). transform;
+
+		InvokeRepeating ("FireControl", 1, 3);
+	}
+	void FireControl()
+	{
+		character.Attack ();
+
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		agent.destination = player.position;
+		transform.LookAt (player.transform);
+	}
+}
---
+++ Assets/Gameplay/Scripts/PlayerControllor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllor : MonoBehaviour
+{
+	PlayerCharacter character;
+	private void Start()
+	{
+
+		character=GetComponent<PlayerCharacter> ();
+
+
+	}
+		
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1"))
+		{
+
+			character.Attack ();
+		
+		}
+
+		var h = Input.GetAxis ("Horizontal");
+		var v = Input.GetAxis ("Vertical");
+		character.Move (new Vector3 (h, 0, v));
+
+		var lookDir = Vector3.forward * v + Vector3.right * h;
+		if (lookDir.magnitude != 0)
+		{
+			character.Rotate (lookDir);
+
+		}
+	}
+}
